844fc2a48dac45ac63b8a3dfbe26a8ba5f153d8e
grace-mao23 <gmao00@stuy.edu>
2019-01-04 14:29:26 -0500

    Added empty methods and fields

---
+++ CalendarViews.java
@@ -0,0 +1,57 @@
+import java.io.*;
+
+public class CalendarViews {
+  private String view;
+  private int month;
+  private int startDate;
+  private int endDate;
+
+  public CalendarViews(String v) {
+
+  }
+
+  public String getView() {
+    return "";
+  }
+
+  public void setView(String s) {
+
+  }
+
+  public String viewDate() { // might not be useful later
+    return "";
+  }
+
+  public String printYear() {
+    return "";
+  }
+
+  public String printMonth() {
+    return "";
+  }
+
+  public String printWeek() {
+    return "";
+  }
+
+  public void getData(File f) {
+
+  }
+
+  public int getFirstDayOfMonth(int month, int year) {
+    return -1;
+  }
+
+  public int getFirstDayOfYear(int year) {
+    return -1;
+  }
+
+  public int getFirstDayOfWeek(int startDate, int month, int year) {
+    return -1;
+  }
+
+  public String getWeekday(int day) {
+    return "";
+  }
+
+}
