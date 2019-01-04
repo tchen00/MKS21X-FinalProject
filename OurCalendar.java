d0c00ce692e1fa86d74340ed70994c980c6d0898
grace-mao23 <gmao00@stuy.edu>
2019-01-03 20:11:54 -0500

    Overall format and empty methods added

--- OurCalendar.java
+++ OurCalendar.java
@@ -1,3 +1,30 @@
+import java.io.*;
+
 public class OurCalendar {
-  
+    private int year;
+    private File data;
+
+    public OurCalendar(File f) {
+
+    }
+
+    public void clearAll() {
+
+    }
+
+    public void createEvent() {
+
+    }
+
+    public void deleteEvent() {
+
+    }
+
+    public void listEvent(String str) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
