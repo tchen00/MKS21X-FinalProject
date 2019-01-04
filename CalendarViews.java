7972929b3958b1d579a32057510f73671c6dd41f
grace-mao23 <gmao00@stuy.edu>
2019-01-04 14:46:47 -0500

    Wrote numberOfDays method

--- CalendarViews.java
+++ CalendarViews.java
@@ -22,6 +22,35 @@ public class CalendarViews {
     return "";
   }
 
+  // returns number of days since Jan 1 2019
+  public int numberOfDays(int day, int month, int year) {
+    int result = 0;
+    // adding days from years
+    for (int i = year - 1; i >= 2019; i--) {
+      if (i % 4 == 0 && !(i % 100 == 0 && i % 400 != 0)) {
+        result += 366;
+      } else {
+        result += 365;
+      }
+    }
+    // THIS IS NOW 2019 (NOT A LEAP YEAR)
+    // adding days from months
+    for (int i = month - 1; i > 0; i--) {
+      int tempDays = 0; // to keep track of how many days are in that month
+      if (i == 12 || i == 10 || i == 8 || i == 7 || i == 5 || i == 3 || i == 1) {
+        tempDays = 31;
+      } else if (i == 2) {
+        tempDays = 28;
+      } else {
+        tempDays = 30;
+      }
+      result += tempDays;
+    }
+    // THIS IS NOT THAT MONTH SPECIFICALLY
+    result += day;
+    return result;
+  }
+
   public String printYear() {
     return "";
   }
@@ -52,6 +81,11 @@ public class CalendarViews {
 
   public String getWeekday(int day) {
     return "";
+  }
+
+  public static void main (String[] args) {
+    CalendarViews test = new CalendarViews("yes");
+    System.out.println(test.numberOfDays(1, 1, 2020));
   }
 
 }
