6e63d74ba3fa35de567bafc211a01d0c084bbaff
Tammy <tchen00@stuy.edu>
2019-01-06 20:48:33 -0500

    test code from online for CSV  reading

--- CSVTesting/CSVTammy.java
+++ CSVTesting/CSVTammy.java
@@ -1,1 +1,38 @@
+import java.io.*;
 
+public class CSVTammy {
+  public static void main(String[] args) throws FileNotFoundException, IOException{
+    String csvFile = "sampleCalendar.csv";
+    BufferedReader br = null;
+    String line = "";
+    String cvsSplitBy = ",";
+
+    try {
+
+        br = new BufferedReader(new FileReader(csvFile));
+        while ((line = br.readLine()) != null) {
+
+            // use comma as separator
+            String[] country = line.split(cvsSplitBy);
+
+            System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+        }
+
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    } finally {
+        if (br != null) {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    }
+
+
+  }
