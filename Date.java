4c8fe4c5a44f8b1ae8cff13d57273453a9e44a03
grace-mao23 <gmao00@stuy.edu>
2019-01-08 14:27:32 -0500

    Wrote convertToMonth to convert ints to the month name

--- Date.java
+++ Date.java
@@ -1,5 +1,21 @@
 public class Date {
 
+  public String convertToMonth(int n) {
+    if (n == 1) return "January";
+    if (n == 2) return "February";
+    if (n == 3) return "March";
+    if (n == 4) return "April";
+    if (n == 5) return "May";
+    if (n == 6) return "June";
+    if (n == 7) return "July";
+    if (n == 8) return "August";
+    if (n == 9) return "September";
+    if (n == 10) return "October";
+    if (n == 11) return "November";
+    if (n == 12) return "December";
+    return null;
+  }
+
   public String convertToDay(int n) {
     if (n == 0) return "Sunday";
     if (n == 1) return "Monday";
