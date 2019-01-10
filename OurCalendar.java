1d4211a653befb6304c1e881d8345e34dd96bbcd
grace-mao23 <gmao00@stuy.edu>
2019-01-10 14:37:37 -0500

    Wrote listEvent without a type for now

--- OurCalendar.java
+++ OurCalendar.java
@@ -4,7 +4,7 @@ public class OurCalendar {
     private int year;
     private File data;
 
-    public OurCalendar(File f) {
+    public OurCalendar(String fileName) {
 
     }
 
@@ -20,11 +20,26 @@ public class OurCalendar {
 
     }
 
-    public void listEvent(String str) {
-
+    public void listEvent(String fileName, String type) throws FileNotFoundException, IOException{
+      // type determines alphabetical vs chronological
+      // for now, we're just printing out the events as seen in the csv file
+      System.out.println("\nYOUR EVENTS: \n");
+      File f = new File(fileName);
+      BufferedReader br = new BufferedReader(new FileReader(f));
+      String line = "";
+      while ((line = br.readLine()) != null) {
+        System.out.println(line);
+      }
+      System.out.println("\n");
     }
 
     public static void main(String[] args) {
+      try {
+        OurCalendar c = new OurCalendar("life.csv");
+        c.listEvent("life.csv","yes");
+      } catch (Exception e) {
+        System.out.println(e);
+      }
 
     }
 }
