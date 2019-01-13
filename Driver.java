9b816675c42b0be25a8f4603ff15990de9c23889
Tammy <tchen00@stuy.edu>
2019-01-13 16:35:06 -0500

    inpu 2

--- Driver.java
+++ Driver.java
@@ -39,10 +39,92 @@ public class Driver {
          inpu = Integer.parseInt(input);
        }
        if (inpu == 2){
+        // CalendarViews test = new CalendarViews();
+         System.out.println("Please select your view option: \n \t 1. Year \n \t 2. Month \n \t 3. Week \n **NOTE: Must be lowercase -- for now**");
+         //try {
+           //System.out.println(test.numberOfDays(28, 2, 2019));
+         //  System.out.println(test.numberOfDays(1, 1, 2018));
+       //    System.out.println(test.numberOfDays(1, 13, 2019));
+         //  System.out.println(test.numberOfDays(1, 0, 2019));
+       //    System.out.println(test.numberOfDays(30, 2, 2020));
+         //  System.out.println(test.numberOfDays(29, 2, 2020));
+       //    System.out.println(test.numberOfDays(29, 2, 2019));
+       //    System.out.println(test.numberOfDays(31, 9, 2019));
+       //    System.out.println(test.numberOfDays(32, 1, 2019));
+       //  } catch (Exception e) {
+         //  System.out.println(e);
+         //}
+     /*    System.out.println(test.numberOfDays(1, 1, 2019));
+         System.out.println(test.numberOfDays(1, 2, 2019));
+         System.out.println(test.numberOfDays(23, 10, 2019));
+         System.out.println(test.numberOfDays(1, 1, 2020));
+         System.out.println(test.numberOfDays(1, 7, 2020));
+         System.out.println(test.numberOfDays(1, 1, 2021)); */
+     /*    System.out.println(test.getFirstDayOfMonth(8, 2019));
+         System.out.println(test.getFirstDayOfMonth(7, 2019));
+         System.out.println(test.getFirstDayOfMonth(6, 2019));
+         System.out.println(test.getFirstDayOfMonth(12, 2019));
+         System.out.println(test.getFirstDayOfMonth(1, 2020));
+         System.out.println(test.getFirstDayOfMonth(5, 2020));
+         System.out.println(test.getFirstDayOfMonth(10, 2023));
+         System.out.println(test.getFirstDayOfYear(2020));
+         System.out.println(test.getFirstDayOfYear(2021));
+         System.out.println(test.getFirstDayOfYear(2022));
+         System.out.println(test.getFirstDayOfYear(2023));
+         System.out.println(test.getFirstDayOfYear(2050)); */
+       /*  System.out.println(test.getWeekday(1,1,2019));
+         System.out.println(test.getWeekday(1,1,2020));
+         System.out.println(test.getWeekday(1,1,2021));
+         System.out.println(test.getWeekday(1,1,2022));
+         System.out.println(test.getWeekday(1,1,2023));
+         System.out.println(test.getWeekday(1,1,2050));*/
+     /*    System.out.println(test.getStartOfWeek(1,1,2019));
+         System.out.println(test.getStartOfWeek(2,1,2019));
+         System.out.println(test.getStartOfWeek(3,1,2019));
+         System.out.println(test.getStartOfWeek(4,1,2019));
+         System.out.println(test.getStartOfWeek(5,1,2019)); */
+         //System.out.println(test.printMonth(1,2019));
+       //  System.out.println(test.printYear(2019));
+     /*    System.out.println(test.printMonth(2,2019));
+         System.out.println(test.printMonth(2,2020));
+         System.out.println(test.printMonth(4,2019));*/
+       /*  try {
+           test.getData("life.csv");
+           System.out.println(test.events.toString());
+         } catch (Exception e) {
+           System.out.println(e);
+         } */
+         String myView = myReader.readLine();
+         if (myView.equals("year")){
+           System.out.println("Please input the year you would like to see: ");
+           String yeary = myReader.readLine();
+           int yearr = Integer.parseInt(yeary);
+           CalendarViews test = new CalendarViews("year","life.csv");
+           System.out.println(test.printYear(yearr));
+         } else if (myView.equals("month")){
+           System.out.println("Please input the year you would like to see: ");
+           String yeary = myReader.readLine();
+           int yearr = Integer.parseInt(yeary);
+           String monthy = myReader.readLine();
+           int monthh = Integer.parseInt(yeary);
+           CalendarViews test = new CalendarViews("month","life.csv");
+           System.out.println(test.printMonth(yearr, monthh));
+         } else if (myView.equals("week")){
+           System.out.println("Please input the year you would like to see: ");
+           String yeary = myReader.readLine();
+           int yearr = Integer.parseInt(yeary);
+           String monthy = myReader.readLine();
+           int monthh = Integer.parseInt(yeary);
+           String dayye = myReader.readLine();
+           int dayy = Integer.parseInt(dayye);
+           CalendarViews test = new CalendarViews("week","life.csv");
+           System.out.println(test.printWeek(yearr, monthh, dayy));
+         }
+         // test.printWeek(7,1,2019);
     //     System.out.print("\033[H\033[2J");
-    //     System.out.flush();  
+    //     System.out.flush();
            // INSERT CODE FOR CALENDAR VIEWS
-           System.out.println("");
+      //     System.out.println("");
       //     String[] arg = {};
     //       CalendarViews.main(arg);
          }
