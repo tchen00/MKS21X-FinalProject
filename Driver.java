c52e2dbc226b0b449f1cd103d1984fec6731e841
Tammy <tchen00@stuy.edu>
2019-01-15 17:51:58 -0500

    tested colors in terminal

--- Driver.java
+++ Driver.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 */
 public class Driver {
 //tested code from online
-/* TESTING COLORS -- UNCOMMENT TO TEST
+// TESTING COLORS -- UNCOMMENT TO TEST
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
@@ -17,14 +17,22 @@ public class Driver {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
-*/
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
   public static void main(String[] args) throws IOException {
     try {
-    /* UNCOMMENT THIS TO TEST COLORS
-       System.out.println(ANSI_RED + "This text is red!" + ANSI_RESET);
-       System.out.println(ANSI_GREEN + "Hi" + ANSI_RESET);
-       System.out.println(ANSI_CYAN + "PERSON!! YOU ARE AMAZING!" + ANSI_RESET);
-    */
+    // UNCOMMENT THIS TO TEST COLORS
+       System.out.println(ANSI_WHITE_BACKGROUND+ ANSI_RED + "This text is red!" + ANSI_RESET);
+       System.out.println(ANSI_YELLOW_BACKGROUND+ ANSI_GREEN + "Hi" + ANSI_RESET);
+       System.out.println(ANSI_PURPLE_BACKGROUND+ ANSI_CYAN + "PERSON!! YOU ARE AMAZING!" + ANSI_RESET);
+
        BufferedReader myReader = new BufferedReader(new InputStreamReader(System.in));
        BufferedReader br = new BufferedReader(new FileReader("life.csv"));
        String csvFile = "life.csv";
