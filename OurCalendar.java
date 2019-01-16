e9cbc310ce1b41bf2a2110fb2e57d947795cd5cb
grace-mao23 <gmao00@stuy.edu>
2019-01-15 19:59:01 -0500

    Added exceptions to filter

--- OurCalendar.java
+++ OurCalendar.java
@@ -25,7 +25,10 @@ public class OurCalendar extends Date {
 
     }
 
-    public ArrayList<Event> filter(ArrayList<Event> e, int month, int year) {
+    public String filter(char type, ArrayList<Event> e, int month, int year) throws FileNotFoundException,IOException {
+      if (!validDate(year, month, 1)) {
+        throw new IllegalArgumentException("Please enter a valid date");
+      }
       ArrayList<Event> result = new ArrayList<Event>();
       for (Event current : e) {
         String date = current.getDate();
@@ -36,33 +39,29 @@ public class OurCalendar extends Date {
           result.add(current);
         }
       }
-      return result;
+      return listEvent(type, result);
     }
 
     // "a" = alphabetical
     // "c" = chronological
     // "n" = order listed in csv file
-    public String listEvent(char type) throws FileNotFoundException, IOException{
+    public String listEvent(char type, ArrayList<Event> e) throws FileNotFoundException, IOException{
+      // e should usually be <someCalendarViewsthing>.getEvents()
       // type determines alphabetical vs chronological
       // for now, we're just printing out the events as seen in the csv file
       String result = "\nYOUR EVENTS: \n\n";
-      CalendarViews c = new CalendarViews("list",file);
-      ArrayList<Event> sorted = new ArrayList<Event>();
-      sorted = c.getEvents();
       if (type == 'a') {
-        Date.insertionSortA(sorted);
+        Date.insertionSortA(e);
       }
       if (type == 'c') {
-        Date.insertionSortC(sorted);
+        Date.insertionSortC(e);
       }
-      for (Event e : sorted) {
-        result += e.toString() + "\n\n";
+      for (Event current : e) {
+        result += current.toString() + "\n\n";
       }
       return result;
     }
 
     public static void main(String[] args) throws IOException {
-      OurCalendar c = new OurCalendar("yayyy.csv");
-      c.clearAll();
     }
 }
